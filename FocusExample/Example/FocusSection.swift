26633b2ee50c80805ab77d94d6f882ef96e03b32
Loc.Nguyen <loc9493@gmaill.com>
2022-10-14 17:26:16 +0700

    add focusSection

---
+++ FocusExample/Example/FocusSection.swift
@@ -0,0 +1,20 @@
+//
+//  FocusSection.swift
+//  FocusExample
+//
+//  Created by Nguyen Loc on 14/10/2022.
+//
+
+import SwiftUI
+
+struct FocusSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FocusSection_Previews: PreviewProvider {
+    static var previews: some View {
+        FocusSection()
+    }
+}
