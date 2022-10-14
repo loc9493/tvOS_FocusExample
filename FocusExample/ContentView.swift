0e951d57f5e919a6802ac8b254937cd060351cac
Loc.Nguyen <loc9493@gmaill.com>
2022-10-14 16:42:06 +0700

    first commit

---
+++ FocusExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FocusExample
+//
+//  Created by Nguyen Loc on 14/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FocusExample/FocusExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusExampleApp.swift
+//  FocusExample
+//
+//  Created by Nguyen Loc on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FocusExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
